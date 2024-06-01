dd5038a8c6e41334972cdeafa4fc43d76dd3bf06
aether1010 <infinitum_studios@outlook.com>
2024-06-01 15:59:57 +0800

    Change to WebClient

--- infMCModInstaller/Program.cs
+++ infMCModInstaller/Program.cs
@@ -4,11 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using RestSharp;
-using AltoHttp;
-using System.Text.RegularExpressions;
-using AppKit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace infMCModInstaller
 {
@@ -16,24 +16,7 @@ namespace infMCModInstaller
     {
         public static void Main(string[] args)
         {
-            HttpDownloader httpDownloader;
-            
             string downloadTargetDirectory = string.Empty;
-
-            // var nsop = new NSOpenPanel();
-            // nsop.Title = "请选择你要安装至的模组文件夹目录";
-            // nsop.CanChooseFiles = false;
-            // nsop.CanChooseDirectories = true;
-            // nsop.AllowsMultipleSelection = false;
-            //
-            // if (nsop.RunModal() == (long)NSModalResponse.OK && nsop.Urls.Length > 0)
-            // {
-            //     downloadTargetDirectory = nsop.Urls[0].Path;
-            // }
-            // else
-            // {
-            //     Console.WriteLine("No valid directory selected");
-            // }
             
             Console.WriteLine("请输入你要安装至的模组文件夹目录");
             downloadTargetDirectory = Console.ReadLine();
@@ -56,8 +39,8 @@ namespace infMCModInstaller
             // string hash = "99658ba320539c41e4c4c4de5f2e8c1f40985bff";
             // string mUrl = $"https://api.modrinth.com/v2/version_file/{hash}";
 
-            string modId = "59621";
-            string fileId = "3990329";
+            string modId = "519860";
+            string fileId = "3457539";
             
             string cUrl = $"https://api.curseforge.com/v1/mods/{modId}/files/{fileId}";
             string cAuthKey = "$2a$10$Kd.G.ZYJVXBDaDjX27Z.GumBfck7sldsODAe8utmnap8i00VBu3dS";
@@ -71,18 +54,20 @@ namespace infMCModInstaller
             request.AddHeader("x-api-key", cAuthKey);
             
             string response = client.Get(request).Content;
-            // string pattern = "\"url\":\"(?<url>[^\"]+)\"";
-            string pattern = "\"downloadUrl\":\"(?<url>[^\"]+)\"";
             
-            Match match = Regex.Match(response, pattern);
-            if (match.Success)
-            {
-                downloadLink = match.Groups["url"].Value;
-                Console.WriteLine(downloadLink);
-            }
+            // JObject jsonResponse = JObject.Parse(response);
+            // downloadLink = jsonResponse["files"][0]["url"].ToString();
+
+            JObject jsonResponse = JObject.Parse(response);
+            downloadLink = jsonResponse["data"]["downloadUrl"].ToString();
+            // Console.WriteLine(jsonResponse.ToString());
+
+            Console.WriteLine(downloadLink);
+
+            WebClient webClient = new WebClient();
+            webClient.Credentials = CredentialCache.DefaultCredentials;
+            webClient.DownloadFile(downloadLink, downloadTargetDirectory + "/" + Path.GetFileName(downloadLink));
             
-            httpDownloader = new HttpDownloader(downloadLink, $"{downloadTargetDirectory}/{Path.GetFileName(downloadLink)}");
-            httpDownloader.Start();
             
             Console.WriteLine("安装完毕! ");
         }
